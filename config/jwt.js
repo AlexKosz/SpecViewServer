248571e9ef2bc6bc12a9a2c994d9c747d936dd67
Alex Koszuta <alexkoszuta43@gmail.com>
2025-05-06 14:02:16 -0400

    jwt authen enhancements and safety

--- config/jwt.js
+++ config/jwt.js
@@ -5,11 +5,17 @@ const secret = process.env.SECRET || 'ABC';
 module.exports.secret = secret;
 
 module.exports.authen = (req, res, next) => {
-  jwt.verify(req.cookies.usertoken, secret, (err) => {
+  if (!req.cookies.usertoken) {
+    return res.status(401).json({ verified: false, message: 'No token provided' });
+  }
+
+  // Return the jwt.verify callback
+  return jwt.verify(req.cookies.usertoken, secret, (err, decoded) => {
     if (err) {
-      res.status(401).json({ verified: false });
-    } else {
-      next();
+      return res.status(401).json({ verified: false, message: 'Invalid token' });
     }
+
+    req.user = decoded;
+    return next();
   });
 };
