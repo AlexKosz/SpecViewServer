bb0fee0f9ba7147d4021724fe82acadb3bf2c6e1
Alex Koszuta <alexkoszuta43@gmail.com>
2025-05-07 14:14:08 -0400

    purge unused files

--- routes/location.routes.js
+++
@@ -1,8 +0,0 @@
-const Locations = require('../controllers/location.controller');
-const { authen } = require('../config/jwt');
-
-module.exports = (app) => {
-  app.post('/locations/read', authen, Locations.getLocations);
-  app.post('/location/create', authen, Locations.addLocation);
-  app.post('/location/read', authen, Locations.getLocation);
-};
